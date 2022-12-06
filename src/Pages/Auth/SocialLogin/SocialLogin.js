114796503534c34e0e321eaf457af4db751af2e0
ashraf_bmqa <ashraful.islam@bengalmobileqa.com>
2022-12-06 19:12:15 +0600

    social login redirect issue fix

--- src/Pages/Auth/SocialLogin/SocialLogin.js
+++ src/Pages/Auth/SocialLogin/SocialLogin.js
@@ -1,18 +1,22 @@
 import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import React, { useContext } from "react";
 import { FaGoogle, FaGithub } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 
 const SocialLogin = () => {
   const { googleLogin, githubLogin } = useContext(AuthContext);
   const googleProvider = new GoogleAuthProvider();
   const githubProvider = new GithubAuthProvider();
-
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
   const handleGoogleLogin = () => {
     googleLogin(googleProvider)
       .then((result) => {
         const user = result.user;
         console.log(user);
+        navigate(from, { replace: true });
       })
       .catch((error) => console.error(error));
   };
@@ -20,6 +24,7 @@ const SocialLogin = () => {
   const handleGithubLogin = () => {
     githubLogin(githubProvider).then((result) => {
       const user = result.user;
+      navigate(from, { replace: true });
       console.log(user);
     });
   };
